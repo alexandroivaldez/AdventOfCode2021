5409122a6a995751ab4ebf4db857602df7231c2a
alexandroivaldez <alexandroivaldez@gmail.com>
2021-12-06 19:28:24 -0600

    Updated recursive function
    This is in NO way optimal or efficient code, but it got the job done for the time limit I had haha. I plan to come back and simplify it if I have extra time.

--- Day3/dayThreePartTwo.py
+++ Day3/dayThreePartTwo.py
@@ -4,7 +4,7 @@ gammaRate = []
 epsilonRate = []
 
 #Store input in list
-with open("test.txt") as file:
+with open("puzzleInput.txt") as file:
     for line in file:
       instruction = line.rstrip()
       arr.append(instruction)
@@ -51,19 +51,86 @@ def binaryConverter(arr):
 globalCounter = 0
 
 def recursiveLoop(arr):
-  print(arr)
+  #print(arr)
+  newNewArr = arr
   if len(arr) == 1:
-    return 1
+    return newNewArr
   else:
     global globalCounter
     newArr = []
+    zeroCount = 0
+    oneCount = 0
+    masterKey = 0
     for instruction in arr:
       code = list(instruction)
-      #print("".join(code))
       if code[globalCounter] == '1':
-        newArr.append(instruction)
-    globalCounter += 1
-    return recursiveLoop(newArr)
+        oneCount += 1
+      else:
+        zeroCount += 1
     
-        
-recursiveLoop(arr)
+    if oneCount >= zeroCount:
+      #print("Ones win!")
+      masterKey = 1
+    else:
+      #print("Zero wins!")
+      masterKey = 0
+
+    for instruction in arr:
+      code = list(instruction)
+      if int(code[globalCounter]) == masterKey:
+        newArr.append(instruction)
+
+    globalCounter += 1
+    #print(newArr)
+    return recursiveLoop(newArr)
+    #return print(str(oneCount) + " " + str(zeroCount))
+
+
+x = recursiveLoop(arr)
+oxygenRating = str(binaryConverter(list(x[0])))
+#print("The oxygen rating is: " + oxygenRating)
+
+co2Rating = 0
+globalCounter2 = 0
+
+def recursiveLoop2(arr):
+  #print(arr)
+  newNewArr = arr
+  if len(arr) == 1:
+    return newNewArr
+  else:
+    global globalCounter2
+    newArr = []
+    zeroCount = 0
+    oneCount = 0
+    masterKey = 0
+    for instruction in arr:
+      code = list(instruction)
+      if code[globalCounter2] == '1':
+        oneCount += 1
+      else:
+        zeroCount += 1
+    
+    if zeroCount <= oneCount:
+      #print("Zero win!")
+      masterKey = 0
+    else:
+      #print("One wins!")
+      masterKey = 1
+
+    for instruction in arr:
+      code = list(instruction)
+      if int(code[globalCounter2]) == masterKey:
+        newArr.append(instruction)
+
+    globalCounter2 += 1
+    #print(newArr)
+    return recursiveLoop2(newArr)
+    #return print(str(oneCount) + " " + str(zeroCount))
+
+y = recursiveLoop2(arr)
+co2Rating = str(binaryConverter(list(y[0])))
+#print("The oxygen rating is: " + co2Rating)
+
+lifeSupportRating = int(oxygenRating) * int(co2Rating)
+print("The life support rating is: " + str(lifeSupportRating))
